407d333698bf4c597303119c59959787ac0214f7
clem <clem@email.com>
2026-02-02 07:52:06 -0800

    add i3 and rofi config

--- home.nix
+++ home.nix
@@ -20,6 +20,7 @@
     neovim
 
     # gui programs
+    rofi
     alacritty
     mpv
     zathura
@@ -34,6 +35,8 @@
   };
 
   home.file = {
+    ".config/i3".source = ./configs/i3;
+    ".config/rofi".source = ./configs/rofi;
     ".config/alacritty".source = ./configs/alacritty;
     ".config/zathura".source = ./configs/zathura;
   };
